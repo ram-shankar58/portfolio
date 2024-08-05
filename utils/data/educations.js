91487eefbf93fa10554f11d3b52153389bcae3ad
R Ram Shankar <sanjayramshankar@gmail.com>
2024-08-05 09:31:43 +0530

    shit works

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "BTech. Computer Science and Engineering",
+    duration: "2022 - 2026(Expected)",
+    institution: "Vellore Institute of Technology - Chennai Campus",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2020 - 2022",
+    institution: "Atomic Energy Central School, Anupuram",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Machine Learning Intern',
+    company: "STEMTEC AI and Robotics",
+    duration: "(April 2024 - July 2024)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Software Developer Intern",
+    company: "Indira Gandhi Centre for Atomic Research, Kalpakkam",
+    duration: "(June 2024 - July 2024)"
   }
 ]
